fc889cef6d6c189fd6e92a17716aaef2ea781f3d
DimitarDKirov <dimkirov@hotmail.com>
2016-07-29 01:49:13 +0300

    Homework Test Driven Development task 1 ready

---
+++ Topics/02. Test-Driven Development/homewrok/Poker.Test/CardTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Poker.Test
+{
+    [TestClass]
+    public class CardTest
+    {
+        [TestMethod]
+        public void Card_ToStringShouldWork()
+        {
+            Card card = new Card(CardFace.Ace, CardSuit.Hearts);
+            var expectedResult = "Ace.Hearts";
+            Assert.AreEqual(expectedResult, card.ToString());
+        }
+    }
+}
