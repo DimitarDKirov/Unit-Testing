2325ff164dabd0cb366b1d69f250f43ced846c2d
DimitarDKirov <dimkirov@gmail.com>
2016-08-03 23:51:47 +0300

    Added hmework mocking

---
+++ Topics/03. Mocking and JustMock/homework/Cars.Tests.JustMock/Mocks/MoqCarsRepository.cs
@@ -0,0 +1,27 @@
+﻿namespace Cars.Tests.JustMock.Mocks
+{
+    using Cars.Contracts;
+    using Cars.Models;
+    using Moq;
+    using System.Linq;
+
+    public class MoqCarsRepository : CarRepositoryMock, ICarsRepositoryMock
+    {
+        protected override void ArrangeCarsRepositoryMock()
+        {
+            //existing mocks
+            var mockedCarsRepository = new Mock<ICarsRepository>();
+            mockedCarsRepository.Setup(r => r.Add(It.IsAny<Car>())).Verifiable();
+            mockedCarsRepository.Setup(r => r.All()).Returns(this.FakeCarCollection);
+            mockedCarsRepository.Setup(r => r.Search(It.IsAny<string>())).Returns(this.FakeCarCollection.Where(c => c.Make == "BMW").ToList());
+            mockedCarsRepository.Setup(r => r.GetById(It.IsAny<int>())).Returns(this.FakeCarCollection.First());
+
+            //new mocks
+            mockedCarsRepository.Setup(r => r.Remove(It.IsAny<Car>())).Verifiable();
+            mockedCarsRepository.Setup(r => r.SortedByMake()).Returns(this.FakeCarCollection.OrderBy(c => c.Make).ToList());
+            mockedCarsRepository.Setup(r => r.SortedByYear()).Returns(this.FakeCarCollection.OrderBy(c => c.Year).ToList());
+            mockedCarsRepository.Setup(r => r.GetById(It.Is<int>(i => i >= 100))).Returns(() => null);
+            this.CarsData = mockedCarsRepository.Object;
+        }
+    }
+}
